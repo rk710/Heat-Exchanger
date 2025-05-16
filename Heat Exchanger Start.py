d2be6a85ae9b42709bf5edf3dc10fde02fa96c44
rk710 <rk710@cam.ac.uk>
2025-05-16 14:27:16 +0100

    Hydraulic analysis complete

--- Heat Exchanger Start.py
+++ Heat Exchanger Start.py
@@ -1,54 +1,55 @@
 import numpy as np
+import math
 
 #Hydraulic Design
 T_cold_in = 293.15
 T_hot_in = 333.15
-Cp = 
-rho_w = 
-k_w = 
-mu =
-Pr = 
-k_tube =
-L =
-d_sh =
-d_noz =
+Cp = 4179
+rho_w = 990.1
+k_w = 0.632
+mu = 0.000651
+Pr = 4.31
+k_tube = 386
+L = 0.35 #Can be modified
+d_sh = 0.064
+d_noz = 0.02
 d_i = 0.006
 d_o = 0.008
-N = 
-N_B =
-B = N_B/L
-Y =
+N = 13 #Number of tubes, can be modified
+N_B = 9 #no. of baffles
+B = L/(N_B+1) #baffle spacing
+Y = 0.014 #Pitch, can be modified
 
 #Hot side analysis
-m_dot_1 = 
-m_dot_2 = 
+m_dot_1 =  #Initial guess
+m_dot_2 = 0.45 #Initial guess
 m_dot_tube = m_dot_2/N
 v_tube = m_dot_tube*4/(rho_w*np.pi*d_i**2)
-Re_tube = rho_w*v_tube*d_i/mu #check if d_i or L
+Re_tube = rho_w*v_tube*d_i/mu
 v_noz_2 = m_dot_2*4/(rho_w*np.pi*d_noz**2)
-f=(1.82*log(Re_tube, 10) - 1.64)**-2
-delta_p_tube = 
-sigma = 
-K_c =
-K_e =
+f=(1.82*math.log(Re_tube, 10) - 1.64)**-2
+delta_p_tube = 0.5 * rho_w * v_tube**2 * f * L/d_i
+sigma = N*d_i**2/d_sh**2
+K_c = 0.45
+K_e = 0.8 #Obtain K_c and K_e from figure 8 in handout, function of sigma hence are just constant
 delta_p_ends = 0.5*rho_w*v_tube**2 * (K_c + K_e)
-delta_p_noz_2 = 
+delta_p_noz_2 = rho_w * v_noz_2**2
 delta_p_2 = delta_p_tube + delta_p_ends + delta_p_noz_2
 
 #Cold side analysis
-A_sh = 
-v_sh =
-d_chic_sh = #Varies with number of passes
-Re_sh = 
-alpha = #0.2 triangular pitch, 0.34 square
+A_sh = d_sh/Y * (Y-d_o) * B
+v_sh = m_dot_1 / (rho_w*A_sh)
+d_chic_sh = d_sh * A_sh*4/(np.pi*d_sh**2) #Varies with number of passes
+Re_sh = rho_w * v_sh * d_chic_sh / mu
+alpha = 0.34 #0.2 triangular pitch, 0.34 square
 delta_p_sh = 4*alpha*Re_sh**-0.15 *N*rho_w*v_sh**2
-v_noz_1 = 
-delta_p_noz_1 = 
+v_noz_1 = m_dot_1*4/ (rho_w*np.pi*d_noz**2)
+delta_p_noz_1 = rho_w * v_noz_1**2
 delta_p_1 = delta_p_sh + delta_p_noz_1
 #Need iterative process to find optimal mass flow rate
 
 #Thermal analysis
-Nu_i =
+Nu_i = 
 Nu_o = 
 #Reynolds no. adjustment?
 h_i =
